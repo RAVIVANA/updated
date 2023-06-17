829a121377ea6aeb5a3d7e09c268028d944c914e
RAVIVANA <136336583+RAVIVANA@users.noreply.github.com>
2023-06-17 07:50:06 +0530

    Add files via upload

---
+++ src/com/nkxgen/spring/jdbc/model/CustomerDaoInterface.java
@@ -0,0 +1,20 @@
+package com.nkxgen.spring.jdbc.model;
+
+import java.util.List;
+
+public interface CustomerDaoInterface {
+
+	void save_Trail(Customertrail customer);
+
+	Customertrail getCustomerById(Long id);
+
+	void save(Customer customer);
+
+	List<Customer> getAllCustomers();
+
+	void updateCustomerDataById(Customer updatedCustomer);
+
+	void deleteCusById(Customertrail cus);
+
+	Customer getRealCustomerById(Long customerId);
+}
